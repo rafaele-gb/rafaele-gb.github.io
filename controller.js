1a15de8dd99a83f496609e38c997e0627ff33184
rafaele-gb <66499565+rafaele-gb@users.noreply.github.com>
2020-07-21 21:01:16 -0700

    finished js function properly
    gotta get details done now!

--- controller.js
+++ controller.js
@@ -1,5 +1,26 @@
 var app = angular.module("rafael", []);
-app.controller("ctrl", function($scope){
-  console.log("message");
-  $scope.name="rafael";
+app.controller("ctrl", function($scope) {
+  $scope.open1 = false;
+  $scope.open2 = false;
+  $scope.open3 = false;
+
+  $scope.revealCard = function(cardNumber) {
+    if (cardNumber == 1) {
+      $scope.open1 = true;
+      $scope.open2 = false;
+      $scope.open3 = false;
+    }
+
+    if (cardNumber == 2) {
+      $scope.open1 = false;
+      $scope.open2 = true;
+      $scope.open3 = false;
+    }
+
+    if (cardNumber == 3) {
+      $scope.open1 = false;
+      $scope.open2 = false;
+      $scope.open3 = true;
+    }
+  }
 });
