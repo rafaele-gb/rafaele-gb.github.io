6637a8dcc92911f871c803adf9a97ce9fbddf29e
rafaele-gb <66499565+rafaele-gb@users.noreply.github.com>
2020-07-23 01:38:38 -0700

    LIGHTBOX - ICON TRANSITION(home)

--- controller.js
+++ controller.js
@@ -3,24 +3,35 @@ app.controller("ctrl", function($scope) {
   $scope.open1 = false;
   $scope.open2 = false;
   $scope.open3 = false;
+  $scope.open4 = false;
 
   $scope.revealCard = function(cardNumber) {
     if (cardNumber == 1) {
       $scope.open1 = true;
       $scope.open2 = false;
       $scope.open3 = false;
+      $scope.open4 = false;
     }
 
     if (cardNumber == 2) {
       $scope.open1 = false;
       $scope.open2 = true;
       $scope.open3 = false;
+      $scope.open4 = false;
     }
 
     if (cardNumber == 3) {
       $scope.open1 = false;
       $scope.open2 = false;
       $scope.open3 = true;
+      $scope.open4 = false;
+    }
+
+    if (cardNumber == 4) {
+      $scope.open1 = false;
+      $scope.open2 = false;
+      $scope.open3 = false;
+      $scope.open4 = true;
     }
   }
 });
